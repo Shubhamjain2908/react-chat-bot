2be288e35e3252938a5bc07eb4dd7115f2998817
Shubham Jain <shubham.jain@engineerbabu.in>
2019-10-08 01:33:54 +0530

    changed Chatbot from functional component to class component

--- src/components/chatbot/Chatbot.js
+++ src/components/chatbot/Chatbot.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Chatbot = () => (
-    <div>
-        Chatbot goes here!!!
-        </div>
-);
+class Chatbot extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            messgaes: []
+        }
+    }
+
+    render() {
+        return (
+            <div style={{ height: 400, width: 400, float: 'right' }}>
+                <div id="chatbot" style={{ height: '100%', width: '100%', overflow: 'auto' }}>
+                    <h2>Chatbot</h2>
+                    <input type="text" />
+                </div>
+            </div>
+        );
+    }
+
+}
 
 export default Chatbot;

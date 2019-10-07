20057af894ba7b756b0fbbef920ee57cc9ede53a
Shubham Jain <shubham.jain@engineerbabu.in>
2019-10-08 00:49:29 +0530

    created app, landing & about components

---
+++ src/components/pages/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div>
+            <hi>Selling you great stuff!!!</hi>
+            with the help of the chatbot
+        </div>
+    )
+};
+
+export default Landing;
